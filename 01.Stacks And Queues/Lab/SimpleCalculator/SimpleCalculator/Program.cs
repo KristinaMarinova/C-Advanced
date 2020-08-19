d9f96dfa136249125c3d09b892acca1eddd23a89
Kristina <kriska2000@abv.bg>
2020-08-19 11:52:52 +0300

    some exercises from Lab

---
+++ 01.Stacks And Queues/Lab/SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program
+{
+    public static void Main()
+    {
+        string[] inputTokens = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        Stack<string> calcExpression = new Stack<string>(inputTokens.Reverse());
+
+        int result = 0;
+        int leftOperand = int.Parse(calcExpression.Pop());
+        while (calcExpression.Count > 1)
+        {
+            string @operator = calcExpression.Pop();
+            int rightOperand = int.Parse(calcExpression.Pop());
+
+            switch (@operator)
+            {
+                case "+":
+                    result = leftOperand + rightOperand;
+                    break;
+                case "-":
+                    result = leftOperand - rightOperand;
+                    break;
+            }
+
+            leftOperand = result;
+        }
+        Console.WriteLine(result);
+    }
+}
