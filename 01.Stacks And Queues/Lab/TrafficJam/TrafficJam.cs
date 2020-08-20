114967431ce1e117d1231b03aa0f0a4020353aa1
Kristina <kriska2000@abv.bg>
2020-08-20 20:05:23 +0300

    Traffic Jam solved

---
+++ 01.Stacks And Queues/Lab/TrafficJam/TrafficJam.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class TrafficJam
+{
+    public static void Main()
+    {
+        int numberOfCarsToPass = int.Parse(Console.ReadLine());
+        string command = Console.ReadLine();
+
+        int carsPassed = 0;
+        Queue<string> cars = new Queue<string>();
+        while (command != "end")
+        {
+            switch (command)
+            {
+                case "green":
+                    for (int i = 0; i < numberOfCarsToPass; i++)
+                    {
+                        if (cars.Count > 0)
+                        {
+                            Console.WriteLine($"{cars.Dequeue()} passed!");
+                            carsPassed++;
+                        }
+                    }
+
+                    break;
+                default:
+                    cars.Enqueue(command);
+                    break;
+            }
+
+            command = Console.ReadLine();
+        }
+
+        Console.WriteLine($"{carsPassed} cars passed the crossroads.");
+    }
+}
