45c4197844aa9b3ea68012534d0455a813592e89
Kristina <kriska2000@abv.bg>
2020-08-20 20:03:59 +0300

    Hot Potatoe solved

---
+++ 01.Stacks And Queues/Lab/HotPotatoe/HotPotatoe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotPotatoe
+{
+    class HotPotatoe
+    {
+        public static void Main()
+        {
+            string[] children = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int tosses = int.Parse(Console.ReadLine());
+
+            Queue<string> theRing = new Queue<string>(children);
+            while (theRing.Count > 1)
+            {
+                for (int i = 1; i < tosses; i++)
+                {
+                    theRing.Enqueue(theRing.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {theRing.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {theRing.Dequeue()}");
+        }
+    }
+}
