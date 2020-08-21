e219ab4c0ebb23ffefc5fbbe5e15cd83bdb1218e
Kristina <kriska2000@abv.bg>
2020-08-21 22:30:21 +0300

    solved task Squares In Matrix

---
+++ 02.MultidimensionalArrays/Exercise/SquaresInMatrix/SquaresInMatrix.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+public class SquaresInMatrix
+{
+    public static void Main()
+    {
+        int[] matrixSizes = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        int rows = matrixSizes[0];
+        int cols = matrixSizes[1];
+
+        char[,] matrix = new char[rows, cols];
+
+        for (int row = 0; row < rows; row++)
+        {
+            char[] rowValues = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(char.Parse)
+                .ToArray();
+
+            for (int col = 0; col < cols; col++)
+            {
+                matrix[row, col] = rowValues[col];
+            }
+        }
+
+        int squares = 0;
+        for (int i = 0; i < matrix.GetLength(0) - 1; i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+            {
+                if (matrix[i, j] == matrix[i, j + 1] &&
+                    matrix[i, j] == matrix[i + 1, j] &&
+                    matrix[i, j] == matrix[i + 1, j + 1])
+                {
+                    squares++;
+                }
+            }
+        }
+
+        Console.WriteLine(squares);
+    }
+}
