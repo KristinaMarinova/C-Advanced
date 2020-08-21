7923d1a38831d1256e04b96909bdd6ceebaaa561
Kristina <kriska2000@abv.bg>
2020-08-21 22:31:12 +0300

    Completed Snake Moves task

---
+++ 02.MultidimensionalArrays/Exercise/SnakeMoves/SnakeMoves.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+public class SnakeMoves
+{
+    public static void Main()
+    {
+        int[] sizes = Console.ReadLine()
+                        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+        int rows = sizes[0];
+        int cols = sizes[1];
+
+        string snake = Console.ReadLine();
+
+        char[,] field = new char[rows, cols];
+        int snakeCharIndex = 0;
+        for (int i = 0; i < rows; i++)
+        {
+            if (i % 2 == 0)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (snakeCharIndex >= snake.Length)
+                    {
+                        snakeCharIndex = 0;
+                    }
+
+                    field[i, j] = snake[snakeCharIndex];
+                    snakeCharIndex++;
+                }
+            }
+            else
+            {
+                for (int j = cols - 1; j >= 0; j--)
+                {
+                    if (snakeCharIndex >= snake.Length)
+                    {
+                        snakeCharIndex = 0;
+                    }
+
+                    field[i, j] = snake[snakeCharIndex];
+                    snakeCharIndex++;
+                }
+            }
+        }
+
+        Print(field);
+    }
+
+    private static void Print(char[,] field)
+    {
+        for (int i = 0; i < field.GetLength(0); i++)
+        {
+            for (int j = 0; j < field.GetLength(1); j++)
+            {
+                Console.Write(field[i, j]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
