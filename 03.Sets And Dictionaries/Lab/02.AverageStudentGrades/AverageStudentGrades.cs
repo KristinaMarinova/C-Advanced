85fe875ecbcc67e79bd2f1012a733510fcbf7b73
Kristina <kriska2000@abv.bg>
2020-08-23 11:36:33 +0300

    Completed AverageStudentGrades task

---
+++ 03.Sets And Dictionaries/Lab/02.AverageStudentGrades/AverageStudentGrades.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class AverageStudentGrades
+    {
+        public static void Main()
+        {
+            int numberOfRecords = int.Parse(Console.ReadLine());
+
+            var studentsRecords = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                string[] inputTokens = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string studentName = inputTokens[0];
+                double studentGrade = double.Parse(inputTokens[1]);
+
+                if (studentsRecords.ContainsKey(studentName))
+                {
+                    studentsRecords[studentName].Add(studentGrade);
+                }
+                else
+                {
+                    studentsRecords.Add(studentName, new List<double>() { studentGrade });
+                }
+            }
+
+            foreach (var record in studentsRecords)
+            {
+                string studentName = record.Key;
+                string studentGrades = string.Join(' ', record.Value.Select(r => r.ToString($"{0:f2}")));
+                double average = record.Value.Average();
+
+                Console.WriteLine($"{studentName} -> {studentGrades} (avg: {average:F2})");
+            }
+        }
+    }
