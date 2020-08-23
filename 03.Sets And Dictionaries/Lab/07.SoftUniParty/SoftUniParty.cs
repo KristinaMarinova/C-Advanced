f87fc559dd6c1d801d32eef6144bd2b9822c53da
Kristina <kriska2000@abv.bg>
2020-08-23 11:39:14 +0300

    Completed SoftUniParty task

---
+++ 03.Sets And Dictionaries/Lab/07.SoftUniParty/SoftUniParty.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+public class SoftuniParty
+{
+    public static void Main()
+    {
+        var reqularGuests = new HashSet<string>();
+        var vipGuests = new HashSet<string>();
+
+        while (true)
+        {
+            string guest = Console.ReadLine();
+            if (guest == "PARTY")
+            {
+                break;
+            }
+
+            if (char.IsDigit(guest[0]))
+            {
+                vipGuests.Add(guest);
+            }
+            else
+            {
+                reqularGuests.Add(guest);
+            }
+        }
+
+        while (true)
+        {
+            string guest = Console.ReadLine();
+            if (guest == "END")
+            {
+                break;
+            }
+
+            if (char.IsDigit(guest[0]))
+            {
+                vipGuests.Remove(guest);
+            }
+            else
+            {
+                reqularGuests.Remove(guest);
+            }
+        }
+
+        Console.WriteLine(vipGuests.Count + reqularGuests.Count);
+
+        if (vipGuests.Count > 0)
+        {
+            foreach (var guest in vipGuests)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+
+        if (reqularGuests.Count > 0)
+        {
+            foreach (var guest in reqularGuests)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+    }
+}
