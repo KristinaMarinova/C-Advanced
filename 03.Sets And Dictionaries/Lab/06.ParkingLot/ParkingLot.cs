7eb8ec098955ca8b1463a6fa6f22c3f2bab23b29
Kristina <kriska2000@abv.bg>
2020-08-23 11:38:45 +0300

    COmpleted ParkingLot task

---
+++ 03.Sets And Dictionaries/Lab/06.ParkingLot/ParkingLot.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+public class ParkingLot
+{
+    public static void Main()
+    {
+        var parkingLot = new HashSet<string>();
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input == "END")
+            {
+                break;
+            }
+
+            string[] inputTokens = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            string direction = inputTokens[0];
+            string carPlate = inputTokens[1];
+
+            switch (direction)
+            {
+                case "IN":
+                    parkingLot.Add(carPlate);
+                    break;
+                case "OUT":
+                    parkingLot.Remove(carPlate);
+                    break;
+            }
+        }
+
+        if (parkingLot.Count == 0)
+        {
+            Console.WriteLine("Parking Lot is Empty");
+        }
+        else
+        {
+            foreach (var carNumber in parkingLot)
+            {
+                Console.WriteLine(carNumber);
+            }
+        }
+    }
+}
