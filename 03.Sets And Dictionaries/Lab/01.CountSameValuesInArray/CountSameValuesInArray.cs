c39638725e8a409557b47a0422695fa65d93f6e4
Kristina <kriska2000@abv.bg>
2020-08-23 11:35:39 +0300

    Completed CountSameValuesInArray task

---
+++ 03.Sets And Dictionaries/Lab/01.CountSameValuesInArray/CountSameValuesInArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class CountSameValuesInArray
+{
+    static void Main()
+    {
+        var inputArr = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(double.Parse)
+            .ToArray();
+
+        var occurences = new Dictionary<double, int>();
+
+        foreach (var number in inputArr)
+        {
+            if (occurences.ContainsKey(number))
+            {
+                occurences[number]++;
+            }
+            else
+            {
+                occurences[number] = 1;
+            }
+        }
+
+        foreach (var occurence in occurences)
+        {
+            Console.WriteLine($"{occurence.Key} - {occurence.Value} times");
+        }
+    }
+}
