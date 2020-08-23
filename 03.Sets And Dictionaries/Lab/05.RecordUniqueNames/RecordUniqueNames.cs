2bb6de8d0c916f272cd733bb7187a18a76fb70ac
Kristina <kriska2000@abv.bg>
2020-08-23 11:38:10 +0300

    Completed RecordUniqueNames task

---
+++ 03.Sets And Dictionaries/Lab/05.RecordUniqueNames/RecordUniqueNames.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+public class RecordUniqueNames
+{
+    public static void Main()
+    {
+        int namesCount = int.Parse(Console.ReadLine());
+        var uniqueNames = new HashSet<string>();
+
+        for (int i = 0; i < namesCount; i++)
+        {
+            string name = Console.ReadLine();
+            uniqueNames.Add(name);
+        }
+
+        foreach (var name in uniqueNames)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
