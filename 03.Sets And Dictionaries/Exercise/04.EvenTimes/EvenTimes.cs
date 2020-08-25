f780939b5fc82a735fd5048b9924cd3ecebd2db3
Kristina <kriska2000@abv.bg>
2020-08-25 09:30:12 +0300

    task completed

---
+++ 03.Sets And Dictionaries/Exercise/04.EvenTimes/EvenTimes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class EvenTimes
+{
+    public static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        Dictionary<int, int> numbersAndCount = new Dictionary<int, int>();
+        for (int i = 0; i < N; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (numbersAndCount.ContainsKey(number))
+            {
+                numbersAndCount[number]++;
+            }
+            else
+            {
+                numbersAndCount.Add(number, 1);
+            }
+        }
+
+        Console.WriteLine(numbersAndCount
+                            .Where(n => n.Value % 2 == 0)
+                            .First()
+                            .Key);
+    }
+}
