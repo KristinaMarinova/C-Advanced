31b8a0c5188ffce933f214b9f64b5250a5608bc0
Kristina <kriska2000@abv.bg>
2020-08-26 21:02:43 +0300

    FolderSize

---
+++ 04.Streams Files And Directories/Lab/FolderSize/FolderSize.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace FolderSize
+{
+    public class FolderSize
+    {
+        public static void Main()
+        {
+            string directory = @"..\..\..\..\Resources\06. Folder Size\TestFolder";
+
+            string[] allFiles = Directory.GetFiles(directory, "*", SearchOption.TopDirectoryOnly);
+
+            double totalFilesSizeMiB = 0d;
+            foreach (var file in allFiles)
+            {
+                using (var reader = new FileStream(file, FileMode.Open, FileAccess.Read))
+                {
+                    double fileSizeMiB = reader.Length / 1_048_576.0;
+                    totalFilesSizeMiB += fileSizeMiB;
+                }
+            }
+
+            string outputPath = @"..\..\..\..\Resources\06. Folder Size\output.txt";
+            using (var writer = new StreamWriter(outputPath))
+            {
+                writer.WriteLine(totalFilesSizeMiB);
+            }
+        }
+    }
+}
